9f5c21e466b55a2057dea22b42549755d4e6de65
Bob Xu <Bob.Xu@nike.com>
2019-05-09 21:28:47 +0800

    Initial commit

---
+++ src/main/java/com/yibo/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.yibo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaClient
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
